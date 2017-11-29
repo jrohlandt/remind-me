700bdfc87e1bb813a62b37934659c921edc0c430
Jeandre Rohlandt <jrohlandt@gmail.com>
2017-11-29 22:39:45 +0200

    prompt for date and convert mont abreviation e.g. jul to int e.g. 7

--- datetime.c
+++ datetime.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 int main ()
 {
+
+    char month_string[3];
+    char *months[12] = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+
+    struct tm dt;
+    
+    printf("Enter date (e.g. 14 apr 2017 13:20) :");
+    scanf("%d %3s %d %d:%d", &dt.tm_mday, month_string, &dt.tm_year, &dt.tm_hour, &dt.tm_min );
+
+    int i;
+    for (i = 0; i < 12; i++) {
+        if (strcmp(months[i], month_string) == 0) {
+            dt.tm_mon = i + 1;
+            break;
+        }
+    }
+
+    
+    printf("Date: %02d-%02d-%d %02d:%02d\n", dt.tm_mday, dt.tm_mon, dt.tm_year, dt.tm_hour, dt.tm_min); 
+
+   // struct date dt;
+    
+  //  printf("Enter date (e.g. 22 7 2017 13:20");
+   // scanf("%d%d%d", &dt.da_day, &dt.da_mon, &dt.da_year );
+
+   // printf("Date: %d-%d-%d\n", dt.da_day, dt.da_mon, dt.da_year);    
+
+    return 0;
+
     time_t now_timestamp;
     now_timestamp = time(NULL);
     char *today = ctime(&now_timestamp);
